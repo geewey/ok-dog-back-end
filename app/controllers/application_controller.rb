27cb0b12082e3267406ae94585f8f5c53e902d50
geewey <geewey@gmail.com>
2020-01-21 15:34:02 +0000

    implemented test route and controller action for receiving POST weather requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,91 +1,108 @@
 class ApplicationController < ActionController::API
 
-    require 'news-api'
-    require 'open-uri'
-    require "google/cloud/dialogflow"
-    # require 'wit'
+  require 'news-api'
+  require 'open-uri'
+  require "google/cloud/dialogflow"
+  # require 'wit'
 
-    ### Snippet for parsing queries when passing params
+  ### Snippet for parsing queries when passing params
 
-    def dialogflow
-        query = params[:query]
-        project_id = "ok-dog-sfhrlg"
-        session_id = "123456789"
-        language_code = "en-US"
+  def dialogflow
+    query = params[:query]
+    project_id = "ok-dog-sfhrlg"
+    session_id = "123456789"
+    language_code = "en-US"
 
-        session_client = Google::Cloud::Dialogflow::Sessions.new
-        session = session_client.class.session_path project_id, session_id
-        
-        query_input = { text: { text: query, language_code: language_code } }
-        response = session_client.detect_intent session, query_input
-        
-        # returns JSON object
-        query_result = response.query_result
-        text_to_post = { dialogflow_response: query_result.fulfillment_text }
-
-        render json: text_to_post
-    end
-
-    # def yelp
-    #     yelp = ENV["YELP_API_KEY"]
+    session_client = Google::Cloud::Dialogflow::Sessions.new
+    session = session_client.class.session_path project_id, session_id
     
-    #     query = params[:query].gsub('"', '')
-    #     location = query.split('_')[0]
-    #     category = query.split('_')[1]
-    #     yelp_url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=#{category}&location=#{location}"
-    #     res = HTTParty.get(yelp_url, :headers => {"Authorization" => "Bearer #{yelp}", "x-requested-with" => "XMLHttpRequest"})
-    #     render plain: res.body.squish
-    # end
+    query_input = { text: { text: query, language_code: language_code } }
+    response = session_client.detect_intent session, query_input
+    
+    # returns JSON object
+    query_result = response.query_result
+    # puts query_result
+    text_to_post = { dialogflow_response: query_result.fulfillment_text }
 
-    def wit
-        # fetch from API
-        Wit.init
-        # resp = Wit.text_query('Hello!', ENV["WIT_API_TOKEN"] <-- error, temp solution is to hardcode)
-        # p "Response: " + resp
-        Wit.close
-        # render json: render_to_page.to_json()
-        render json: resp.to_json()
-    end
+    render json: text_to_post
+  end
 
-    def error
-        error_response = {error: "Sorry, I don't understand. Please try again."}
-        render json: error_response
-    end
+  # def yelp
+    # yelp = ENV["YELP_API_KEY"]
 
-    def news
-        news_api_key = ENV["NEWS_API_KEY"]
-        
-        n = News.new(news_api_key)
-        bbc_news_top_headlines = n.get_top_headlines(sources: "bbc-news")
-        first_bbc_news_headline = bbc_news_top_headlines[0]
-        render json: first_bbc_news_headline.to_json()
+    # query = params[:query].gsub('"', '')
+    # location = query.split('_')[0]
+    # category = query.split('_')[1]
+    # yelp_url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=#{category}&location=#{location}"
+    # res = HTTParty.get(yelp_url, :headers => {"Authorization" => "Bearer #{yelp}", "x-requested-with" => "XMLHttpRequest"})
+    # render plain: res.body.squish
+  # end
 
-    end
+  def wit
+    # fetch from API
+    Wit.init
+    # resp = Wit.text_query('Hello!', ENV["WIT_API_TOKEN"] <-- error, temp solution is to hardcode)
+    # p "Response: " + resp
+    Wit.close
+    # render json: render_to_page.to_json()
+    render json: resp.to_json()
+  end
 
-    def joke
-        # returns JSON object
-        url = 'https://icanhazdadjoke.com/'
-        req = open(url, {'Accept' => 'application/json','User-Agent' => 'Flatiron Student Project (https://github.com/geewey/)' })
-        response_body = req.read
-        json_response_body = JSON(response_body)
-        joke_json_response_body = json_response_body.select{ |k, v| k == "joke" }
-        # joke = JSON(response_body)["joke"]
-        # render json: joke.to_json()
-        render json: joke_json_response_body
-    end
+  def error
+    error_response = {error: "Sorry, I don't understand. Please try again."}
+    render json: error_response
+  end
 
-    def weather
-        # returns JSON object
-        weather_api_key = ENV["WEATHER_API_KEY"]
-        
-        url = 'api.openweathermap.org'
-        city_name = 'London'
-        country_code = 'uk'
+  def news
+    news_api_key = ENV["NEWS_API_KEY"]
+    
+    n = News.new(news_api_key)
+    bbc_news_top_headlines = n.get_top_headlines(sources: "bbc-news")
+    first_bbc_news_headline = bbc_news_top_headlines[0]
+    render json: first_bbc_news_headline.to_json()
 
-        req = open("http://api.openweathermap.org/data/2.5/weather?q=
-            #{city_name},#{country_code}&APPID=#{weather_api_key}")
-        response_body = req.read
-        render json: response_body
-    end
+  end
+
+  def joke
+    # returns JSON object
+    url = 'https://icanhazdadjoke.com/'
+    req = open(url, {'Accept' => 'application/json','User-Agent' => 'Flatiron Student Project (https://github.com/geewey/)' })
+    response_body = req.read
+    json_response_body = JSON(response_body)
+    joke_json_response_body = json_response_body.select{ |k, v| k == "joke" }
+    # joke = JSON(response_body)["joke"]
+    # render json: joke.to_json()
+    render json: joke_json_response_body
+  end
+
+  def weather_get
+    # returns JSON object
+    weather_api_key = ENV["WEATHER_API_KEY"]
+
+    url = 'api.openweathermap.org'
+    city_name = 'London'
+    country_code = 'uk'
+
+    req = open("http://api.openweathermap.org/data/2.5/weather?q=
+      #{city_name},#{country_code}&APPID=#{weather_api_key}")
+    response_body = req.read
+    render json: response_body
+  end
+
+  def weather_post
+    
+    puts request.body.read
+    puts "***************"
+    puts request.raw_post
+
+    # url = 'api.openweathermap.org'
+    # city_name = 'London'
+    # country_code = 'uk'
+
+    # req = open("http://api.openweathermap.org/data/2.5/weather?q=
+    #   #{city_name},#{country_code}&APPID=#{weather_api_key}")
+    # response_body = req.read
+    # render json: response_body
+  end
 
 end
